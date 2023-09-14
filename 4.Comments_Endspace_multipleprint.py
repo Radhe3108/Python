5e8a982f9fcec96298d9d4bb57346989d48d7ef0
radhe <134047417+Radhe3108@users.noreply.github.com>
2023-09-14 19:52:13 +0530

    Create 4.Comments_Endspace_multipleprint.py

---
+++ 4.Comments_Endspace_multipleprint.py
@@ -0,0 +1,23 @@
+#This is python program
+
+print("Hey") #This is print method
+
+# This is
+# Multi-line comments
+
+# OR
+
+"""
+This is
+Multi-Line comments
+"""
+
+
+# Escape Sequence Characters
+
+print("Hello \n Hey")
+# print("This doesnt "execute")  
+print("This will \" execute")
+
+#More on print Statement
+print("Hey",5,6,sep="~",end="009")
