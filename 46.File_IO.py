20bf35828b0c9b61f8e070e93de7413168522348
radhe <134047417+Radhe3108@users.noreply.github.com>
2023-10-07 19:59:43 +0530

    Create 46.File_IO.py

---
+++ 46.File_IO.py
@@ -0,0 +1,18 @@
+# READING A FILE
+
+# f = open('myfile.txt', 'r')
+# # print(f)
+# text = f.read()
+# print(text)
+# f.close()
+
+# WRITING A FILE
+
+# f = open('myfile.txt', 'a')
+# f.write('Hello, world!')
+# f.close()
+
+
+# "with" helps to not specify close function
+# with open('myfile.txt', 'a') as f:
+#   f.write("Hey I am inside with")
