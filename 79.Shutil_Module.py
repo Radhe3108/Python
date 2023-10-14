5178dbe704a6fa809b19ecb31378844c79630005
radhe <134047417+Radhe3108@users.noreply.github.com>
2023-10-14 13:37:27 +0530

    Create 79.Shutil_Module.py

---
+++ 79.Shutil_Module.py
@@ -0,0 +1,9 @@
+import shutil
+import os
+
+# shutil.copy("main.py", "main2.py")
+# shutil.copytree(".tutorial", "mytutorial")
+# shutil.move(".tutorial/file.file", "file.file")
+# shutil.rmtree("mytutorial")
+
+os.remove("file.file")
